e06ecf738b99e6233190aaec905b3fa0528b0d58
harshtiwari <harshtiwari8833@gmail.com>
2023-06-24 14:57:48 +0530

    new

---
+++ app/src/main/java/com/pixelperfect/testharsh.java
@@ -0,0 +1,14 @@
+package com.pixelperfect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class testharsh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testharsh);
+    }
+}
