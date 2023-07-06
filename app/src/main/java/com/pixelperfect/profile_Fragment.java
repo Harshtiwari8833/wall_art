6e7930d387ea9486baa7806e8e68619cdd4ab9dc
nikhilpanchal <nikhil.parasf.9911@gmail.com>
2023-07-06 15:20:23 +0530

    new

--- app/src/main/java/com/pixelperfect/profile_Fragment.java
+++ app/src/main/java/com/pixelperfect/profile_Fragment.java
@@ -52,6 +52,14 @@ public class profile_Fragment extends Fragment {
             }
         });
 
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 = new Intent(getActivity(),SignupActivity.class);
+                startActivity(intent3);
+            }
+        });
+
         rate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,6 +67,7 @@ public class profile_Fragment extends Fragment {
                 Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=one4studio.wallpaper.one4wall");
                 Intent intent2 = new Intent(Intent.ACTION_VIEW,uri);
                 startActivity(intent2);
+
 
             }
         });
