1d9567396b5bb1e47820b497273ac2c031b7b003
nikhilpanchal <nikhil.parasf.9911@gmail.com>
2023-07-09 22:16:44 +0530

    new

--- app/src/main/java/com/pixelperfect/cat_Fragment.java
+++ app/src/main/java/com/pixelperfect/cat_Fragment.java
@@ -3,6 +3,7 @@ package com.pixelperfect;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,6 +22,10 @@ public class cat_Fragment extends Fragment {
    RecyclerView recyclerView;
     ArrayList<catModel> arrayList = new ArrayList<>();
 
+    CardView card_nature, card_city,card_animal, card_landscape,card_amoled,card_dark,
+                card_anime,card_cars, card_sports, card_space,card_superhero,card_ios,
+                    card_solid, card_abstract, card_shapes,card_minimal;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,7 +33,37 @@ public class cat_Fragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_cat_, container, false);
 
-        recyclerView = view.findViewById(R.id.recycle);
+        card_nature = view.findViewById(R.id.card_nature);
+        card_animal = view.findViewById(R.id.card_animal);
+        card_landscape = view.findViewById(R.id.card_landscape);
+        card_amoled = view.findViewById(R.id.card_amoled);
+        card_dark = view.findViewById(R.id.card_dark);
+        card_city = view.findViewById(R.id.card_city);
+        card_anime = view.findViewById(R.id.card_anime);
+        card_cars = view.findViewById(R.id.card_cars);
+        card_sports = view.findViewById(R.id.card_sports);
+        card_space = view.findViewById(R.id.card_space);
+        card_superhero = view.findViewById(R.id.card_superhero);
+        card_ios = view.findViewById(R.id.card_ios);
+        card_solid = view.findViewById(R.id.card_solid);
+        card_abstract = view.findViewById(R.id.card_abstract);
+        card_shapes = view.findViewById(R.id.card_shapes);
+        card_minimal = view.findViewById(R.id.card_minimal);
+
+        card_nature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        return view;
+
+
+
+
+        /*recyclerView = view.findViewById(R.id.recycle);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
         recyclerView.setLayoutManager(linearLayoutManager);
 
@@ -53,9 +88,10 @@ public class cat_Fragment extends Fragment {
    RecyclerAdapterCAT adapter = new RecyclerAdapterCAT(getContext(), arrayList);
 
             recyclerView.setAdapter(adapter);
-            adapter.notifyDataSetChanged();
+            adapter.notifyDataSetChanged();  */
 
-            return view;
+
     }
 
 }
+
