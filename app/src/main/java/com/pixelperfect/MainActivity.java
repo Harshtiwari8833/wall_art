345ffbc7fdf89396d547b3c1b055e5db038d3434
nikhilpanchal <nikhil.parasf.9911@gmail.com>
2023-08-09 16:56:12 +0530

    new

--- app/src/main/java/com/pixelperfect/MainActivity.java
+++ app/src/main/java/com/pixelperfect/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView btnView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
